be5d5d43c76bdac3f5afd7a28fbc0657c2aafbc8
Fraser Wilson <FraserW@discovery.co.za>
2024-08-15 09:28:59 +0200

    First project created

---
+++ I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Fraser Wilson on 2024/08/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
