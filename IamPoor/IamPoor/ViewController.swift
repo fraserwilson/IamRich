50a4f508b8cb5cee31f5259402f84baad7c924fa
Fraser Wilson <FraserW@discovery.co.za>
2024-08-28 12:33:17 +0200

    I am poor version of the app

---
+++ IamPoor/IamPoor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IamPoor
+//
+//  Created by Fraser Wilson on 2024/08/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
